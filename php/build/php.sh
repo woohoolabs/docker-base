83b9ef02b4c67fefb4faf99871dd153a96413ebd
Kocsis Máté <kocsismate@woohoolabs.com>
2016-02-20 22:25:55 +0100

    Added cURL support

--- php/build/php.sh
+++ php/build/php.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker-php-ext-install bcmath gettext json mbstring mysqli opcache pdo_mysql && \
-apt-get -y install zlib1g-dev libicu-dev g++ && \
+apt-get -y install libcurl4-gnutls-dev zlib1g-dev libicu-dev g++ && \
+docker-php-ext-install bcmath curl gettext json mbstring mysqli opcache pdo_mysql && \
 docker-php-ext-configure intl && \
 docker-php-ext-install intl
