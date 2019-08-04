2d7d47e45360cce49c242982b0f0fdef7e699f52
kocsismate <kocsismate@woohoolabs.com>
2019-08-04 10:40:49 +0200

    Cleanup

--- gulp/build/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eux
-
-apt-get clean
-rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* /var/www/*
-
-mkdir -p /code
-chmod -R 775 /code
--- gulp/build/permissions.sh
+++ gulp/build/permissions.sh
@@ -4,8 +4,8 @@ set -eux
 addgroup -g 82 gulp
 adduser -u 82 --system -G gulp gulp
 
-chown -R root:gulp /usr/local/lib/node_modules
+chown -R root:root /usr/local/lib/node_modules
 
 mkdir -p /code
-chown -R root:gulp /code
+chown -R root:root /code
 chmod -R 775 /code
