9e0c207f7bbcacb5ac15cd6b8f9d49dc32db22ba
kocsismate <kocsismate@woohoolabs.com>
2019-08-04 10:30:10 +0200

    Remove unncesessary dependency from PHP images

--- php/build/system.sh
+++ php/build/system.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -eux
 
-apk add --no-cache --repository http://dl-3.alpinelinux.org/alpine/edge/community/ --allow-untrusted \
-    dos2unix
-
 apk add --no-cache \
     curl \
     git \
